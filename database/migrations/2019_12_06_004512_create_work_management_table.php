e18641c364f6a2dfaa1721eee2b18fb758a78422
alafgani1000 <irhabyaganal625@gmail.com>
2019-12-06 15:00:35 +0700

    migrate table

--- database/migrations/2019_12_06_004512_create_work_management_table.php
+++ database/migrations/2019_12_06_004512_create_work_management_table.php
@@ -13,23 +13,43 @@ class CreateWorkManagementTable extends Migration
      */
     public function up()
     {
-        Schema::create('incident', function (Blueprint $table) {
+        Schema::create('incidents', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('asset');
+            $table->string('problem');
+            $table->string('solution');
+            $table->dateTime('solve_at');
             $table->timestamps();
         });
 
-        Schema::create('request', function (Blueprint $table) {
+        Schema::create('requests', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('nopegawai');
+            $table->string('requirment');
+            $table->timestamps();
+        });
+
+        Schema::create('request_details', function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('file_name');
+            $table->string('url');
             $table->timestamps();
         });
 
         Schema::create('todo', function (Blueprint $table){
             $table->bigIncrements('id');
+            $table->bigInteger('request_id');
+            $table->string('title');
             $table->timestamps();
         });
 
         Schema::create('todo_details', function (Blueprint $table){
             $table->bigIncrements('id');
+            $table->bigInteger('todo_id');
+            $table->string('kode');
+            $table->string('text');
+            $table->date('plan_finish_date');
+            $table->date('finish_date');
             $table->timestamps();
         });
     }
